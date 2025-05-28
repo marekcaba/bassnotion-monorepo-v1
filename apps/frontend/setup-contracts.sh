065831cf9525df8527ddbae3867a671795acfb30
marekcaba <marekcaba@seznam.cz>
2025-05-28 17:40:09 +0200

    Build contracts library before copying to fix missing dist folder

--- apps/frontend/setup-contracts.sh
+++ apps/frontend/setup-contracts.sh
@@ -4,6 +4,12 @@ set -e
 echo "Installing npm dependencies..."
 npm install
 
+echo "Building contracts library..."
+cd ../../libs/contracts
+npm install
+npm run build
+cd ../../apps/frontend
+
 echo "Setting up contracts library..."
 mkdir -p node_modules/@bassnotion/contracts
 cp -r ../../libs/contracts/* node_modules/@bassnotion/contracts/
